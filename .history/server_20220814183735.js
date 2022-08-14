5aee7578ab37d05ef16402463d5b75af0740ce5e
Vasu <vasucp1207@gmail.com>
2022-08-14 18:38:51 +0530

    delete the document

--- .history/server_20220814183735.js
+++ .history/server_20220814183735.js
@@ -72,14 +72,6 @@ app.route('/users/:id')
 .delete((req,res)=>{
   User.findByIdAndDelete(
     req.params.id,
-    (err, data) => {
-      if (err){
-        res.json({success: false,message: err})
-      } else if (!data){
-        res.json({success: false,message: "Not Found"})
-      } else {
-        res.json({success: true,data: data})
-      }
-    }
+    if
   )
 })
